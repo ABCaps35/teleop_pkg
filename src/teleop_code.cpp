5a80171a4e9eb9267580027ac98ee470a68c027d
ABCaps35 <abc92@case.edu>
2019-09-17 16:47:02 -0400

    Adds topic name input code to teleop code

--- src/teleop_code.cpp
+++ src/teleop_code.cpp
@@ -3,6 +3,7 @@
 #include "geometry_msgs/Twist.h"
 
 #include <sstream>
+#include <stdlib.h>
 
 void calcNewVel(){
 
@@ -11,6 +12,22 @@ void calcNewVel(){
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "publisher_node");
+	// Process command line parameter looking for a -n string name
+	// and should be placed after the ros::init() invocation.
+	// rosrun <package_name> <executable_name> -n <new_name>
+	// or
+	// rosrun subscriber_package subscriber_node -n alternate_topic
+	int opt;
+	while ((opt = getopt(argc, (argv), "n:")) != -1) {
+	  switch (opt) {
+	    case 'n':
+	      topic_name = optarg;
+	      break;
+	    default:
+	      printf("The -%c is not a recognized parameter\n", opt);
+	      break; 
+	  }
+	}
 	ros::NodeHandle n;
 	ros::Publisher p = n.advertise<geometry_msgs::Twist>("topic_name",1000);
 	ros::Subscriber s = n.subscribe("topic_name",1000,calcNewVel);
